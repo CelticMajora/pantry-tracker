1e99de68f883ddd047f1702e0724d81f15c8ab50
CelticMajora <cdonoho16@gmail.com>
2019-09-23 16:25:38 -0400

    Moved around web stuff and about to mess around with thymeleaf forms

--- src/main/java/com/pantrytracker/app/controllers/WebController.java
+++ src/main/java/com/pantrytracker/app/controllers/WebController.java
@@ -1,4 +1,4 @@
-package com.pantrytracker.app;
+package com.pantrytracker.app.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class PlaygroundController {
+public class WebController {
 	@Value("${spring.application.name}")
 	String appName;
 	
