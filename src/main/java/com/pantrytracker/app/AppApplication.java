781397e8dfb77be97086f150d26ee3e1fa71072b
CelticMajora <cdonoho16@gmail.com>
2019-10-01 00:03:28 -0400

    Trying to get different environments set up, but the Heroku end might be whack

--- src/main/java/com/pantrytracker/app/AppApplication.java
+++ src/main/java/com/pantrytracker/app/AppApplication.java
@@ -2,10 +2,12 @@ package com.pantrytracker.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@PropertySource({"classpath:application-${envTarget:production}.properties"})
 public class AppApplication {
 
 	public static void main(String[] args) {
