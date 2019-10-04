4b52d127c95ade91ac854883c704201d73f83354
Caleb Donoho <cdonoho16@gmail.com>
2019-10-04 13:55:11 -0400

    Fix heroku db issues (#8)
    * Trying to fix the configuration to work with heroku
    
    * Read that postgres uses sequence tables, so need to make that or use generationtype IDENTITY

--- src/main/java/com/pantrytracker/app/entities/UserlessIngredient.java
+++ src/main/java/com/pantrytracker/app/entities/UserlessIngredient.java
@@ -14,7 +14,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 public class UserlessIngredient implements Serializable{
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;	
 	private String email;
 	private String name;
